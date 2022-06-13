a6fa5e2c696ff7ebca026da4404b76ed52411b4f
Sergei <nickincorname@gmail.com>
2022-06-13 12:42:55 +0300

    added hilt, impl, layouts for fragments, nav-graph

---
+++ app/src/main/java/com/example/testproject/ui/fragments/AboutClientFragment.kt
@@ -0,0 +1,25 @@
+package com.example.testproject.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.testproject.R
+
+class AboutClientFragment : Fragment() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_client, container, false)
+    }
+
+
+}
